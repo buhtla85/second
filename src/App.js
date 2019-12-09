77a8781cbc631f544e6e2d348771f133fc235959
buhtla85 <tamara.naunovic@gmail.com>
2019-12-09 20:00:48 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var React = require("react");
+var App = function () {
+    return (React.createElement(React.Fragment, null,
+        React.createElement("div", { className: "primary-color" }, "Hello World!")));
+};
+exports.default = App;
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+
+const App = () => {
+    return (
+        <React.Fragment>
+            <div className="primary-color">Hello World!</div>
+        </React.Fragment>
+    );
+}
+
+export default App;
