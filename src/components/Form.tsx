1b23217e20e94436b63b239d585d9f3c40e5a783
buhtla85 <tamara.naunovic@gmail.com>
2019-12-10 10:58:37 +0100

    added form component

---
+++ src/components/Form.tsx
@@ -0,0 +1,30 @@
+import * as React from "react";
+
+interface IStateForm {
+    cityName: string,
+    countryName: string,
+    errMessage: string
+}
+
+export class Form extends React.Component<{}, IStateForm> {
+    constructor(props:any) {
+        super(props);
+        this.state = {
+            cityName: "", countryName: "", errMessage: ""
+        }
+    }
+    render () {
+        return (
+            <section className="container pt-3">
+                <form action="" className="form-inline d-flex justify-content-center">
+                    <label htmlFor="inlineFormInputCity2" className="sr-only">City</label>
+                    <input type="text" className="form-control mb-2 mr-sm-2" id="inlineFormInputCity2" placeholder="City"/>
+                    <label htmlFor="inlineFormInputCountry2" className="sr-only">Country</label>
+                    <input type="text" className="form-control mb-2 mr-sm-2" id="inlineFormInputCountry2" placeholder="Country"/>
+                    <button type="button" className="btn btn-outline-secondary mb-2">Search</button>
+                </form>
+            </section>
+        );
+    }
+};
+
