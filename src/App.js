323387d9aa65153b7dfef7346dcb9dbe88a7712c
buhtla85 <tamara.naunovic@gmail.com>
2019-12-10 11:27:18 +0100

    added form to render

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var React = require("react");
+var Navbar_1 = require("./components/Navbar");
 var App = function () {
     return (React.createElement(React.Fragment, null,
-        React.createElement("div", { className: "primary-color" }, "Hello World!")));
+        React.createElement(Navbar_1.Navbar, { title: "React TS Weather App" })));
 };
 exports.default = App;
