ef95f2f24a6567ac53c9d50b1a75579ced271ce7
buhtla85 <tamara.naunovic@gmail.com>
2019-12-11 20:30:57 +0100

    added celsius symbol

--- src/components/DisplayWeatherConditions.tsx
+++ src/components/DisplayWeatherConditions.tsx
@@ -17,7 +17,7 @@ export const DisplayWeather = (props: IConditionsProps) => {
     return (
         <section className="d-block text-center pt-3">
             {props.cityName && props.countryName && <h1 className="text-white font-weight-light pb-1">Location: {props.cityName}, {props.countryName}</h1>}
-            {props.temperature && <h3 className="text-white font-weight-light pb-1">Temperature: {props.temperature}</h3>}
+            {props.temperature && <h3 className="text-white font-weight-light pb-1">Temperature: {props.temperature} &#8451;</h3>}
             {props.icon && <img src={`http://openweathermap.org/img/wn/${props.icon}@2x.png`} alt="weather image"/>}
             {props.description && <h3 className="text-white font-weight-light pb-1">Conditions: {props.description}</h3>}
             {props.humidity && <h3 className="text-white font-weight-light pb-1">Humidity: {props.humidity} %</h3>}
