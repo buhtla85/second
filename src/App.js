146413a1c52933452a68f92472c09a393d7407f1
buhtla85 <tamara.naunovic@gmail.com>
2019-12-14 20:27:42 +0100

    delete

--- src/App.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var React = require("react");
-var Navbar_1 = require("./components/Navbar");
-var App = function () {
-    return (React.createElement(React.Fragment, null,
-        React.createElement(Navbar_1.Navbar, { title: "React TS Weather App" })));
-};
-exports.default = App;
